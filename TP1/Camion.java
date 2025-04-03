9f2b1d573cb73c836a4485bd464b5708548ef692
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2025-04-03 20:44:46 -0300

    Añadir clase camion

---
+++ TP1/Camion.java
@@ -0,0 +1,19 @@
+package TP1;
+
+public class Camion  extends Vehiculo{
+    private boolean tieneAcoplado;
+    
+    public Camion(String patente, String marca, int anio, double capacidadCargaKg, boolean tieneAcoplado){
+        super(patente,marca,anio,capacidadCargaKg);
+        this.tieneAcoplado = tieneAcoplado;
+    }
+
+    public boolean getTieneAcoplado(){
+        return tieneAcoplado;
+    }
+
+    public void setTieneAcoplado(boolean tieneAcoplado){
+        this.tieneAcoplado = tieneAcoplado;
+    }
+
+}
