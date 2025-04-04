c0c6bd34b72ab0b647ed744b8bdc41b47f50b729
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2025-04-04 17:17:21 -0300

    Implementar arrayList para gestionar vehiculos

--- TP1/Main.java
+++ TP1/Main.java
@@ -1,21 +1,25 @@
 package TP1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main (String[] args){
-        Vehiculo vehiculo1 = new Vehiculo("ABC123", "Toyota", 2020, 2000);
-        Vehiculo vehiculo2 = new Vehiculo("DEF456", "Fiat", 1994, 1000);
-        Vehiculo vehiculo3 = new Vehiculo("WYZ789", "Ford" ,2005, 1050);
-        Camion camion1 = new Camion("GHI789", "Volvo", 2010, 8000, true);
-        Camion camion2 = new Camion("ERT767", "Audi", 2015, 7500, false);
-        Auto auto1 = new Auto("QWE", "Honda", 2004, 5000, 4);
+        List<Vehiculo> vehiculo = new ArrayList<>();
+        vehiculo.add(new Vehiculo("ABC123", "Toyota", 2020, 2000));
+        vehiculo.add(new Vehiculo("DEF456", "Fiat", 1994, 1000));
+        vehiculo.add(new Vehiculo("WYZ789", "Ford" ,2005, 1050));
+        vehiculo.add(new Camion("GHI789", "Volvo", 2010, 8000, true));
+        vehiculo.add(new Camion("ERT767", "Audi", 2015, 7500, false));
+        vehiculo.add(new Auto("QWE", "Honda", 2004, 5000, 4));
 
         System.out.println("\n📌 Lista de Vehículos Registrados:");
         System.out.println("-----------------------------------");
-        VehiculoPrinter.imprimirVehiculo(vehiculo1);
-        VehiculoPrinter.imprimirVehiculo(vehiculo2);
-        VehiculoPrinter.imprimirVehiculo(vehiculo3);
-        VehiculoPrinter.imprimirVehiculo(camion1);
-        VehiculoPrinter.imprimirVehiculo(camion2);
-        VehiculoPrinter.imprimirVehiculo(auto1);
+        VehiculoPrinter.imprimirVehiculo(vehiculo.get(0));
+        VehiculoPrinter.imprimirVehiculo(vehiculo.get(1));
+        VehiculoPrinter.imprimirVehiculo(vehiculo.get(2));
+        VehiculoPrinter.imprimirVehiculo(vehiculo.get(3));
+        VehiculoPrinter.imprimirVehiculo(vehiculo.get(4));
+        VehiculoPrinter.imprimirVehiculo(vehiculo.get(5));
     }
 }
