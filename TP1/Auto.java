76a47d94a4e78dcd5f52bc686c8da2672903f142
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2025-04-04 15:59:10 -0300

    Validar tratamiento uniforme de vehículos con polimorfismo (LSP)

--- TP1/Auto.java
+++ TP1/Auto.java
@@ -12,12 +12,17 @@ public class Auto extends Vehiculo{
         return cantidadPasajeros;
    }
 
-   public void setCantidadPasajeros(int cantidadPasajeros){
-        if (cantidadPasajeros> 0) {
-            this.cantidadPasajeros = cantidadPasajeros;
-        } else {
-            throw new IllegalArgumentException("Error: La cantidad de pasajeros debe ser mayor a 0.");
-        }
-    }
+   public void setCantidadPasajeros(int cantidadPasajeros) {
+       if (cantidadPasajeros > 0) {
+           this.cantidadPasajeros = cantidadPasajeros;
+       } else {
+           throw new IllegalArgumentException("Error: La cantidad de pasajeros debe ser mayor a 0.");
+       }
+   }
+
+   @Override
+   public String getDescripcion(){
+        return super.getDescripcion() + " | Cantidad de pasajeros: " + cantidadPasajeros;
+   }
 
 }
